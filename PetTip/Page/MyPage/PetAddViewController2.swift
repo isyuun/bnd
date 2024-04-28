c326022571e43834daefd81b1025680b538affec
isyuun@care-biz.co.kr <isyuun@care-biz.co.kr>
2024-04-28 22:35:10 +0900

    몸무게 소수점자리 제한 없음: 텍스트 필드 소스점 첫째자리  설정

---
+++ PetTip/Page/MyPage/PetAddViewController2.swift
@@ -0,0 +1,11 @@
+//
+//  AddPetViewController.swift
+//  PetTip
+//
+//  Created by carebiz on 1/6/24.
+//
+
+import UIKit
+
+class PetAddViewController2: PetAddViewController {
+}
---
+++ PetTip/Page/MyPage/PetModViewController2.swift
@@ -0,0 +1,12 @@
+//
+//  PetModViewController.swift
+//  PetTip
+//
+//  Created by carebiz on 1/28/24.
+//
+
+import UIKit
+import AlamofireImage
+
+class PetModViewController2: PetModViewController {
+}
