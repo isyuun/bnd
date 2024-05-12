86c5e5f9e7848276e1c25131bd52f0bba88d1273
isyuun@care-biz.co.kr <isyuun@care-biz.co.kr>
2024-05-12 18:58:30 +0900

    우측 하단 현재위치정보 버튼 3번 클릭시 현재위치가 사라짐

--- PetTip/Map/NMapViewController3.swift
+++ PetTip/Map/NMapViewController3.swift
@@ -12,8 +12,8 @@ import AVKit
 class NMapViewController3: NMapViewController {
 
     override func viewDidLoad() {
-        super.viewDidLoad()
         NSLog("[LOG][I][(\(#fileID):\(#line))::\(#function)][naverMap:\(String(describing: naverMap))][mapView:\(String(describing: mapView))]")
+        super.viewDidLoad()
 
         btnLocation.layer.cornerRadius = 2.0
         btnLocation.layer.borderColor = UIColor.init(hexCode: "e3e9f2").cgColor
@@ -22,12 +22,49 @@ class NMapViewController3: NMapViewController {
         btnLocation.setImage(UIImage(named: "icon-gps1-bgx"), for: .normal)
         btnLocation.setTitle("", for: .normal)
         btnLocation.showShadowLight()
+
+        mapView.positionMode = .normal
     }
 
     @IBOutlet weak var btnLocation: UIButton!
     @IBAction func onBtnLocation(_ sender: Any) {
-        // NSLog("[LOG][I][(\(#fileID):\(#line))::\(#function)][sender:\(String(describing: sender))]")
-        NSLog("[LOG][I][(\(#fileID):\(#line))::\(#function)][naverMap:\(String(describing: naverMap))][mapView:\(String(describing: mapView))]")
+        NSLog("[LOG][I][(\(#fileID):\(#line))::\(#function)][naverMap.showLocationButton:\(String(describing: naverMap.showLocationButton))][mapView.positionMode:\(String(describing: mapView.positionMode))]")
         naverMap.showLocationButton = false
+
+        switch (mapView.positionMode) {
+        case .disabled:
+            mapView.positionMode = .normal
+            break
+        case .normal:
+            mapView.positionMode = .direction
+            break
+        case .direction:
+            mapView.positionMode = .compass
+            break
+        case .compass:
+            mapView.positionMode = .normal
+            break
+        default:
+            mapView.positionMode = .normal
+            break
+        }
+
+        switch (mapView.positionMode) {
+        case .disabled:
+            btnLocation.setImage(UIImage(named: "icon-gps1"), for: .normal)
+            break
+        case .normal:
+            btnLocation.setImage(UIImage(named: "icon-gps1-bgx"), for: .normal)
+            break
+        case .direction:
+            btnLocation.setImage(UIImage(named: "icon-gps2-bgx"), for: .normal)
+            break
+        case .compass:
+            btnLocation.setImage(UIImage(named: "icon-gps3-bgx"), for: .normal)
+            break
+        default:
+            btnLocation.setImage(UIImage(named: "icon-gps1-bgx"), for: .normal)
+            break
+        }
     }
 }
